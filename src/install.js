d3be7aeac2a402d66bab1d34e6ee7ddc61860300
DanCodes <dan@dancodes.online>
2020-01-06 20:53:31 +0100

    possibly work

--- src/install.js
+++ src/install.js
@@ -1,30 +1,33 @@
 const fs = require("file-system");
 const path = require("path");
-process.platform = "";
-try {
-  fs.unlinkSync(path.resolve(__dirname, "..", "lib", "nova"));
-  switch (process.platform) {
-    case "linux":
-      fs.writeFileSync(
-        path.resolve(__dirname, "..", "lib", "nova"),
-        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-linux"))
-      );
-      break;
-    case "win32":
-      fs.writeFileSync(
-        path.resolve(__dirname, "..", "lib", "nova"),
-        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-win"))
-      );
-      break;
-    case "darwin":
-      fs.writeFileSync(
-        path.resolve(__dirname, "..", "lib", "nova"),
-        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-macos"))
-      );
-      break;
-    default: {
-      console.error("Err: " + " could not find platform.");
-      process.exit(1);
-    }
-  }
-} catch (err) {}
+const downloadRelease = require("download-github-release");
+
+const user = "dan-online";
+const repo = "nova";
+const outputdir = path.resolve(__dirname, "..", "lib");
+const leaveZipped = false;
+const platform =
+  process.platform == "darwin"
+    ? "macos"
+    : process.platform == "linux"
+    ? "linux"
+    : "win";
+
+function filterRelease(release) {
+  return release.prerelease === false;
+}
+
+function filterAsset(asset) {
+  return asset.name.indexOf(platform) >= 0;
+}
+
+downloadRelease(user, repo, outputdir, filterRelease, filterAsset, leaveZipped)
+  .then(function() {
+    fs.copyFileSync(
+      path.resolve(outputdir, "nova-" + platform),
+      path.resolve(outputdir, "nova")
+    );
+  })
+  .catch(function(err) {
+    throw err;
+  });
