b1304d41a8cb1f8f1264fbb7710f483199373994
DanCodes <dan@dancodes.online>
2020-01-06 20:32:46 +0100

    fix install

--- src/install.js
+++ src/install.js
@@ -1,28 +1,30 @@
 const fs = require("file-system");
 const path = require("path");
 process.platform = "";
-fs.unlinkSync(path.resolve(__dirname, "..", "lib", "nova"));
-switch (process.platform) {
-  case "linux":
-    fs.writeFileSync(
-      path.resolve(__dirname, "..", "lib", "nova"),
-      fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-linux"))
-    );
-    break;
-  case "win32":
-    fs.writeFileSync(
-      path.resolve(__dirname, "..", "lib", "nova"),
-      fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-win"))
-    );
-    break;
-  case "darwin":
-    fs.writeFileSync(
-      path.resolve(__dirname, "..", "lib", "nova"),
-      fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-macos"))
-    );
-    break;
-  default: {
-    console.error("Err: " + " could not find platform.");
-    process.exit(1);
+try {
+  fs.unlinkSync(path.resolve(__dirname, "..", "lib", "nova"));
+  switch (process.platform) {
+    case "linux":
+      fs.writeFileSync(
+        path.resolve(__dirname, "..", "lib", "nova"),
+        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-linux"))
+      );
+      break;
+    case "win32":
+      fs.writeFileSync(
+        path.resolve(__dirname, "..", "lib", "nova"),
+        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-win"))
+      );
+      break;
+    case "darwin":
+      fs.writeFileSync(
+        path.resolve(__dirname, "..", "lib", "nova"),
+        fs.readFileSync(path.resolve(__dirname, "..", "lib", "nova-macos"))
+      );
+      break;
+    default: {
+      console.error("Err: " + " could not find platform.");
+      process.exit(1);
+    }
   }
-}
+} catch (err) {}
