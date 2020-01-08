e51a89f915ae9ab5181b82f5239c6e5d93b642f6
DanCodes <dan@dancodes.online>
2020-01-08 10:53:13 +0100

    global var

--- src/utils/parse/handlers/variables.js
+++ src/utils/parse/handlers/variables.js
@@ -1,6 +1,8 @@
 var variables = [
   { key: "args", value: process.argv.slice(process.argv.indexOf("./test.ns")) },
-  { key: "platform", value: process.platform }
+  { key: "platform", value: process.platform },
+  { key: "process", value: { pid: process.pid, exit: process.exit } },
+  { key: "global", value: { directory: process.cwd() } }
 ];
 const { error, debug } = require("../../log/all");
 const safeEval = require("safe-eval");
